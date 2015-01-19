4af08e880a2ae7896ac327285682b39db1bd4057
Jeremy McDermond <nh6z@nh6z.net>
2015-01-18 20:15:42 -0800

    Changed cic.v to be consolidated into one file.  This module now handles both static and variable decimation rates in the CIC.  Also got rid of the support modules and collapsed them into this one.  That makes cic.v a standalone module that can be use for nearly anything.

--- sdrstick_rx/cic.v
+++ sdrstick_rx/cic.v
@@ -21,104 +21,118 @@ Boston, MA  02110-1301, USA.
 //           Copyright (c) 2015 Jeremy McDermond, NH6Z
 //------------------------------------------------------------------------------
 
+// 2013 Jan 26	- Modified to accept decimation values from 1-40. VK6APH 
 
+module cic(decimation, clock, in_strobe,  out_strobe, in_data, out_data );
 
-module cic( clock, in_strobe, out_strobe, in_data, out_data );
+  //design parameters
+  parameter STAGES = 5;
+  parameter MIN_DECIMATION = 2;
+  parameter MAX_DECIMATION = 40;
+  parameter IN_WIDTH = 18;
+  parameter OUT_WIDTH = 18;
 
-//design parameters
-parameter STAGES = 3;
-parameter DECIMATION = 16;  
-parameter IN_WIDTH = 18;
+  // derived parameters
+  parameter ACC_WIDTH = IN_WIDTH + (STAGES * $clog2(MAX_DECIMATION));
+  
+  input [5:0] decimation; 
+  
+  input clock;
+  input in_strobe;
+  output reg out_strobe;
 
-//computed parameters
-//ACC_WIDTH = IN_WIDTH + Ceil(STAGES * Log2(DECIMATION))
-//OUT_WIDTH = IN_WIDTH + Ceil(Log2(DECIMATION) / 2)
-parameter OUT_WIDTH = IN_WIDTH  + 2;
-//parameter ACC_WIDTH = IN_WIDTH  + 12;
-parameter ACC_WIDTH = IN_WIDTH + (STAGES * $clog2(DECIMATION));
+  input signed [IN_WIDTH-1:0] in_data;
+  output reg signed [OUT_WIDTH-1:0] out_data;
 
-input clock;
-input in_strobe;
-output reg out_strobe;
-input signed [IN_WIDTH-1:0] in_data;
-output signed [OUT_WIDTH-1:0] out_data;
 
 //------------------------------------------------------------------------------
 //                               control
 //------------------------------------------------------------------------------
 reg [15:0] sample_no;
-initial sample_no = 15'd0;
+initial sample_no = 16'd0;
 
+task advance_sample_no;
+input [5:0] decimation_factor;
+begin
+	if (sample_no == (decimation_factor - 1)) begin
+		sample_no <= 0;
+		out_strobe <= 1;
+	end else begin
+		sample_no <= sample_no + 8'd1;
+      		out_strobe <= 0;
+	end
+end
+endtask
 
-always @(posedge clock)
-  if (in_strobe)
-    begin
-    if (sample_no == (DECIMATION-1))
-      begin
-      sample_no <= 0;
-      out_strobe <= 1;
-      end
-    else
-      begin
-      sample_no <= sample_no + 8'd1;
-      out_strobe <= 0;
-      end
-    end
-
-  else
-    out_strobe <= 0;
-
-
-
-
-
+generate
+if(MIN_DECIMATION == MAX_DECIMATION) begin
+	always @(posedge clock) begin
+		if (in_strobe) 
+			advance_sample_no(MAX_DECIMATION);
+		else
+			out_strobe <= 0;
+	end
+end else begin
+	always @(posedge clock) begin
+		if (in_strobe) 
+			advance_sample_no(decimation);
+		else
+			out_strobe <= 0;
+	end
+end
+endgenerate
 
 //------------------------------------------------------------------------------
 //                                stages
 //------------------------------------------------------------------------------
-wire signed [ACC_WIDTH-1:0] integrator_data [0:STAGES];
-wire signed [ACC_WIDTH-1:0] comb_data [0:STAGES];
+reg signed [ACC_WIDTH-1:0] integrator_data [0:STAGES];
+reg signed [ACC_WIDTH-1:0] comb_data [0:STAGES];
+reg signed [ACC_WIDTH-1:0] comb_last [0:STAGES];
 
+always @(posedge clock) begin
+	integer index;
 
-assign integrator_data[0] = in_data;
-assign comb_data[0] = integrator_data[STAGES];
+	//  Integrators
+	if(in_strobe) begin
+		integrator_data[1] <= integrator_data[1] + in_data;
+		for(index = 1; index < STAGES; index = index + 1) begin
+			integrator_data[index + 1] <= integrator_data[index] + integrator_data[index+1];
+		end
+	end
 
-
-genvar i;
-generate
-  for (i=0; i<STAGES; i=i+1)
-    begin : cic_stages
-
-    cic_integrator #(ACC_WIDTH) cic_integrator_inst(
-      .clock(clock),
-      .strobe(in_strobe),
-      .in_data(integrator_data[i]),
-      .out_data(integrator_data[i+1])
-      );
-
-
-    cic_comb #(ACC_WIDTH) cic_comb_inst(
-      .clock(clock),
-      .strobe(out_strobe),
-      .in_data(comb_data[i]),
-      .out_data(comb_data[i+1])
-      );
-    end
-endgenerate
-
-
-
-
-
-
+	if(out_strobe) begin
+		comb_data[1] <= integrator_data[STAGES] - comb_last[0];
+		comb_last[0] <= integrator_data[STAGES];
+		for(index = 1; index < STAGES; index = index + 1) begin
+			comb_data[index + 1] <= comb_data[index] - comb_last[index];
+			comb_last[index] <= comb_data[index]; 
+		end
+	end
+end
 
 //------------------------------------------------------------------------------
 //                            output rounding
 //------------------------------------------------------------------------------
-assign out_data = comb_data[STAGES][ACC_WIDTH-1:ACC_WIDTH-OUT_WIDTH] +
-  {{(OUT_WIDTH-1){1'b0}}, comb_data[STAGES][ACC_WIDTH-OUT_WIDTH-1]};
 
-//assign out_data = comb_data[STAGES][36:19] + comb_data[STAGES][18];
+//XXX  Can we use assigns here for the output?
+
+genvar j;
+generate
+	if(MIN_DECIMATION == MAX_DECIMATION) begin
+		always @(posedge clock) begin
+			out_data <= comb_data[STAGES][ACC_WIDTH - 1 -: OUT_WIDTH] + comb_data[STAGES][ACC_WIDTH - OUT_WIDTH - 1];
+		end
+	end else begin
+		wire [$clog2(ACC_WIDTH):0] msb [MAX_DECIMATION + 1];
+		for(j = MIN_DECIMATION; j < MAX_DECIMATION + 1; j = j + 1) begin: round_position
+			assign msb[j] = IN_WIDTH + ($clog2(j) * STAGES) - 1 ;
+		end
+
+		//  Round values
+		always @(posedge clock) begin
+			out_data <= comb_data[STAGES][msb[decimation] -: OUT_WIDTH] + comb_data[STAGES][msb[decimation] - OUT_WIDTH];
+		end
+	end
+endgenerate
 
 endmodule
-
